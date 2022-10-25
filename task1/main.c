2f0effe5a03fb2dfe1495a142f77f08dd266d530
zhangyp108 <592027654@qq.com>
2022-10-25 21:35:13 +0800

    Modify task1

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-	printf("Hello World2!\n");
-	return 0;
+int main()
+{
+	double a,b ;
+	double x = 0;
+	double h = 0.001;
+	
+	printf("ÇëÊäÈëa,¼´y0µÄÖµ:");
+	double y;
+	scanf("%lf",&a);
+	printf("    x\t       y\n");
+	do
+	{
+		
+		x+=0.001;
+		b = a;
+		
+		a*= (1-h+h*h/2);
+		printf("%f   %f\n",x,a);
+	}while((b-a)>=0.001*0.001);
+	
+	
+		
+	
+	
+	
 }
-
